40d2847cbb1526946989beb496abb8b592f6ec98
muribe <muribe@fi.uba.ar>
2025-01-01 17:57:59 -0300

    first commit

---
+++ mysite/blog/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(
+            request,
+            'blog/post/list.html',
+            {'posts': posts}
+            )
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No post found")
+    return render(
+            request,
+            'blog/post/detail.html',
+            {'post': post}
+            )
+
+def post_detail_alt(request, id):
+    post = get_object_or_404(
+            Post,
+            id=id,
+            status=Post.Status.PUBLISHED)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post}
+                  )
