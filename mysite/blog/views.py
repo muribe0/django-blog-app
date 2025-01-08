04bf637ee2ba2b89cab1ca7bde019c8daf54ed51
muribe <muribe@fi.uba.ar>
2025-01-07 22:35:02 -0300

    added pagination and exception handlers for pagination. Also, included the pagination in the list template

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Pagination with 2 posts per page
+    paginator = Paginator(post_list, 2)
+
+    # if there is not a page parameter, display the page 1
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # if the page number is out of range
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        # if the page number is not an integer
+        posts = paginator.page(1)
+
     return render(
             request,
             'blog/post/list.html',
