c23d88496212f0534c31cd0eb1d6a397da9249a4
muribe <muribe@fi.uba.ar>
2025-01-12 20:22:38 -0300

    added id column to posts

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -6,7 +6,7 @@ from .models import Post
 @admin.register(Post)
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_display = ['id', 'title', 'slug', 'author', 'publish', 'status']
     list_filter = ['status', 'created', 'publish', 'author']
     search_fields = ['title', 'body']
     prepopulated_fields = {'slug': ('title',)}
