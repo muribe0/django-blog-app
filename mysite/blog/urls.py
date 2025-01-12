75a4ce80939ce01e7281de176c670cf5fac8d0d5
muribe <muribe@fi.uba.ar>
2025-01-12 20:22:26 -0300

    added share post view which lets you share a post through email

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 urlpatterns = [
         # path('', views.post_list, name='post_list'),
         path('', views.PostListView.as_view(), name='post_list'),
-        path('<slug:slug>/', views.post_detail, name='post_detail')
+        path('<slug:slug>/', views.post_detail, name='post_detail'),
+        path('<int:post_id>/share/', views.post_share, name='post_share'), # TODO: change id to slug
         ]
