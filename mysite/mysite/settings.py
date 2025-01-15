6ba889d98bc87695429fcf57f9b11d4ae80f0b17
muribe <muribe@fi.uba.ar>
2025-01-15 01:54:59 -0300

    minor changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-]
+    'taggit',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
