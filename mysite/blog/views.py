96e7f98afe4e78f6ca17a8c5a966402c396705fb
muribe <muribe@fi.uba.ar>
2025-01-15 01:52:28 -0300

    adde

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
-
+from django.db.models import Count
 from django.views.decorators.http import require_POST
 
 from taggit.models import Tag
@@ -61,10 +61,17 @@ def post_detail(request, slug):
 
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    # Similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     context = {
             'post': post,
             'comments': comments,
-            'form': form
+            'form': form,
+            'similar_posts': similar_posts,
             }
     return render(
             request,
